ef9a73cdcd5ab01b2665c2f9c944e403e9dd7a46
yu <yuzhongtao238910@126.com>
2024-07-04 18:08:24 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+/*
+纯函数：
+  输入是什么，输出就是什么
+  函数内部不能修改函数作用域的外部的变量
+
+  sagas采用生成器的函数来执行yeild effects，包含指令的文本对象
+*/
+function App() {
+	return <h1>238910</h1>
+}
+
+export default App
